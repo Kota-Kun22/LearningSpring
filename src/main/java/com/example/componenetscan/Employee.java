fc7c16f7ab72319f8877c2356680c55bffa1945f
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2025-05-11 20:13:04 +0530

    how value annotation works for now have learned how it works just hardcoded here

--- src/main/java/com/example/componenetscan/Employee.java
+++ src/main/java/com/example/componenetscan/Employee.java
@@ -1,13 +1,20 @@
 package com.example.componenetscan;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Employee {
 
+    @Value("harsh")
     private String name;
+
+    @Value("211070")
     private int empId;
+
+    @Value("100000")
     private double salary;
+
 
     public String getName() {
         return name;
