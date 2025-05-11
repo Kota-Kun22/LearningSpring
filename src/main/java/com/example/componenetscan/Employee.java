11ea4e3521757f3b456efef639af445418ad8e9f
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2025-05-11 20:06:19 +0530

    how component annotation works and how component scanner works

---
+++ src/main/java/com/example/componenetscan/Employee.java
@@ -0,0 +1,44 @@
+package com.example.componenetscan;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+    private String name;
+    private int empId;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", empId=" + empId +
+                ", salary=" + salary +
+                '}';
+    }
+}
